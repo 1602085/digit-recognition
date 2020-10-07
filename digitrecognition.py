b14f01dc633025988eabcd7a7f487845bc14f84d
Shikha verma <39978663+1602085@users.noreply.github.com>
2020-10-07 13:31:51 +0530

    Update digitrecognition.py

--- digitrecognition.py
+++ digitrecognition.py
@@ -58,14 +58,6 @@ def softmax_layer(a):
     return d
 
 
-cell_size = (6,6)
-a = cell_size[0]
-b = cell_size[1]
-num_block = 4
-votes = np.zeros(9)
-
-
-
 num_iter = 200
 alpha = 0.02
 input11 = []
@@ -73,12 +65,13 @@ mini_batch = 0
 pred_out = []
 
 
-def Backpropogation(input1, output, w1, w2, w3, w4, alpha, mini_batch=0): #num_iter, mini_batch, final_feature=[]):
+def Backpropogation(input1, output, w1, w2, w3, w4, mini_batch=0): 
     for k in range(200):  
         for i in range(mini_batch, mini_batch+300):
             inp11 = input1[i]
-            out = output[i]
             inp = feature(inp11)
+            out = output[i]
+            
             hidden_layer1 = np.dot(inp, w1)
  
             hidden_layer11 = []   
@@ -99,14 +92,13 @@ def Backpropogation(input1, output, w1, w2, w3, w4, alpha, mini_batch=0): #num_i
 
             output_layer = np.dot(np.array(hidden_layer33).reshape(1,10), w4).astype(int)
             output_layer_mean = output_layer.mean()
-            output_layer = output_layer / output_layer_mean
+            output_layer = output_layer / output_layer_mean #if you don't put this, then softmax_layer give nan values
             
             out_layer = softmax_layer(output_layer)
             print(out_layer)
             output1 = max(out_layer)
             pred_out.append(output1)
-   
- 
+  
             l = len(pred_out)
 
             output_error = (np.array(pred_out[l-1]) - np.array(out))*np.array(hidden_layer33).reshape(10,1)
@@ -124,20 +116,17 @@ def Backpropogation(input1, output, w1, w2, w3, w4, alpha, mini_batch=0): #num_i
             w1 = w1-alpha*h_layer1
 
         mini_batch = mini_batch + 300
-    print(mini_batch)
     return w4, w3, w2, w1
 
 
-w4, w3, w2, w1 = Backpropogation(input1, output, w1, w2, w3, w4, alpha, mini_batch=0)
+w4, w3, w2, w1 = Backpropogation(input1, output, w1, w2, w3, w4, mini_batch=0)
 print(w4)
 print(w3)
 print(w2)
 print(w1)
    
-
-#print(pred_out)
+    
 #pred_out = np.array(pred_out, dtype=uint8)
-
 
 #total=0
 #for i in range(len(output)):
